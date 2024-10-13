64ec84b8f8ae939bb174b79a1361a876c0533fb1
FuranisClay <2924364957@qq.com>
2024-10-13 11:02:12 +0800

    lifetime

--- exercises/lifetimes/lifetimes1.rs
+++ exercises/lifetimes/lifetimes1.rs
@@ -8,9 +8,10 @@
 // Execute `rustlings hint lifetimes1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+//
 
-fn longest(x: &str, y: &str) -> &str {
+//返回的值至少要和传入的活的一样长，如果原始被释放了，那么返回的值就是一个悬垂指针，为了避免这种情况，我们需要使用生命周期
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
